03053a4566f102e02693c824c7a73fce37cc3fdc
Philippe Spino <spip2401@usherbrooke.ca>
2017-05-23 13:28:30 -0400

    Finished Ballon-Mousse part

--- Matlab/Minuterie.m
+++ Matlab/Minuterie.m
@@ -14,7 +14,7 @@ vbprime = e.*(Vp-Vb) + vpprime;
 
 eq1 = (mb.*Vb + mp.*Vp) == (mb.*vbprime + mp.*vpprime);
 r1 = solve(eq1,vpprime);
-disp(double(r1));
+%disp(double(r1));
 
 %% Ballon Attrape
 %e = 0
@@ -22,12 +22,16 @@ disp(double(r1));
 syms Vpb;
 eq2 = (mb.*Vb + mp.*Vp) == (mb + mp).*Vpb;
 Vpb = solve(eq2,Vpb);
-disp(double(Vpb));
+%disp(double(Vpb));
 
 %% Calcul du deltaT.
 %Lors de la fusion du participant et du ballon.
 
 syms dt;
 eq3 = Vpb.*dt == 3.0;
-dt = double(solve(eq3,dt));
-disp((dt - 0.02));       %marge de manoeuvre
+dt1 = double(solve(eq3,dt));
+disp(dt1); 
+eq4 = r1.*dt == 3.0;
+dt2 = double(solve(eq4,dt));
+disp(dt2);
+
