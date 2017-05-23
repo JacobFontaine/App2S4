07639503e2900a4306c1ec7ca1bdd7f6895a9d33
AXEL-PC\axelb <axel.bosco@gmail.com>
2017-05-23 14:19:52 -0400

    Update m=3 to m=5

--- Matlab/Ouverture_valve.m
+++ Matlab/Ouverture_valve.m
@@ -3,9 +3,9 @@ close all;
 
 xp = [0 10 20 30 40 50 60 70 80 90 100];
 yf = [0.87 0.78 0.71 0.61 0.62 0.51 0.51 0.49 0.46 0.48 0.46];
-NmoinsM = 8;
+NmoinsM = 6;
 A = ApproximationLineaire(xp, yf, NmoinsM);
 syms x;
-eqn = A(1) + A(2).*x + A(3).*x.^2 == 0.6296;
+eqn = A(1) + A(2).*x + A(3).*x.^2 + A(4).*x.^3 + A(5).*x.^4 == 0.6296;
 solx = solve(eqn,x,'MaxDegree', 4);
 disp(abs(double(solx)));
