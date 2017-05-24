72cc2906cfa8faca02713622a41e0f79df2b393a
AXEL-PC\axelb <axel.bosco@gmail.com>
2017-05-23 23:51:48 -0400

    Finished Vitesse sans frottement, Ordre et Coeff., Coeff mu f and Pourcentage sections

--- Matlab/Vitesse_participant.m
+++ Matlab/Vitesse_participant.m
@@ -22,7 +22,7 @@ vf = sqrt(((Ep0-Epf).*2)/m);
 Ecf = 0.5.*m.*Ve.^2;
  
 W = Ep0 + Ec0 - Epf - Ecf ;
-u= -(W / (m * -g * 25));
+u= (W / (m * g * 25));
  
     NmoinsM =0;
     A = ApproximationLineaire(xp, yf, NmoinsM);
